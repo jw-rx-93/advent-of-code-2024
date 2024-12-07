7346b6386fda11913396bba0bd3c85f5cd06e99c
Jian <jxw7410@rit.edu>
2024-12-07 13:02:58 -0500

    comments; also benchmarking

--- day7/day7.rb
+++ day7/day7.rb
@@ -1,5 +1,6 @@
 
 require 'set'
+require 'benchmark'
 PREFIX = "test"
 
 def extract_data_keys
@@ -22,11 +23,13 @@ end
 DATA =  extract_data_keys
 
 
-def build_tree(dataset, currentNum, currentIdx, targetNum, concat = false)
-  $stored.add(targetNum) if targetNum == currentNum  && currentIdx == dataset.length - 1
- 
+def permutate(dataset, currentNum, currentIdx, targetNum, concat = false)
+
   next_index = currentIdx + 1
-  return if next_index >= dataset.length 
+  if next_index >= dataset.length 
+    $stored.add(targetNum) if targetNum == currentNum
+    return 
+  end
 
   next_val = dataset[next_index]
   next_plus = currentNum + next_val
@@ -38,9 +41,9 @@ def build_tree(dataset, currentNum, currentIdx, targetNum, concat = false)
   return if !stillPossible && (next_plus == targetNum || next_mul == targetNum || next_concat == targetNum) 
   #end
 
-  build_tree(dataset, next_plus, currentIdx + 1, targetNum, concat) if next_plus <= targetNum
-  build_tree(dataset, next_mul, currentIdx + 1, targetNum, concat) if next_mul <= targetNum
-  build_tree(dataset, next_concat, currentIdx + 1, targetNum, concat) if concat && next_concat <= targetNum
+  permutate(dataset, next_plus, currentIdx + 1, targetNum, concat) if next_plus <= targetNum
+  permutate(dataset, next_mul, currentIdx + 1, targetNum, concat) if next_mul <= targetNum
+  permutate(dataset, next_concat, currentIdx + 1, targetNum, concat) if concat && next_concat <= targetNum
 end
 
 
@@ -48,17 +51,16 @@ end
 def solution1
   DATA.each do |dataset|
     target = dataset[0]
-    build_tree(dataset, dataset[1], 1, target)
+    permutate(dataset, dataset[1], 1, target)
   end 
 
   $stored.to_a.inject(0, :+)
 end 
 
 def solution2 
- 
   DATA.each do |dataset|
     target = dataset[0]
-    build_tree(dataset, dataset[1], 1, target, true)
+    permutate(dataset, dataset[1], 1, target, true)
   end 
 
   $stored.to_a.inject(0, :+)
@@ -68,11 +70,13 @@ end
 $stored = Set.new([])
 puts solution1
 $stored = Set.new([])
-puts solution2
+t = Benchmark.measure do 
+ puts solution2
+end 
 
+puts t.real
 =begin
   Essentially the problem can be thought off as a tree with branches leading to next operation. So we can effectively permutate the solution.
-  We have the option to do a dfs afterwards but it is not neccessary since we can just stored relevant numbers in a set when building the tree.
 
   We can do slight optimizations by adding conditions that make sense, for example, if you get your target sum, and if you are not at the end of your array,
   then you have to check if the remaining numbers are 1s otherwise it is not possible to solve. [This can be leveraged further if you are really good at math, 
